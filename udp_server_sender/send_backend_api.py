3ffb3c4e0ddb199cef4333a629390323c768cdfb
mail.david82@gmail.com <mail.david82@gmail.com>
2024-12-12 17:10:25 +0100

    send api test

--- udp_server_sender/send_backend_api.py
+++ udp_server_sender/send_backend_api.py
@@ -1,12 +1,25 @@
+from flask import Flask, request, jsonify
 from sendPackage import UDPSender
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
+@app.route('/open_door', methods=['POST'])
+def open_door():
+    boardID = request.args.get('boardID')
+    doorID = request.args.get('doorID')
+    if not boardID or not doorID:
+        return jsonify({"error": "Missing boardID or doorID"}), 400
+
+    inputvar = f"{boardID},{doorID}"
     udp = UDPSender()
     try:
         udp.close()
     except:
         pass
-    udp.open("8988228066603096577")
-    udp.send(command="Open Door", inputvar="0,11")
+    udp.open(boardID)
+    udp.send(command="Open Door", inputvar=inputvar)
     udp.close()
+    return jsonify({"status": "success"}), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
