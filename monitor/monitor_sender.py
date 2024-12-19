0f42d213bf07a0b2a91ced0d7e77be888c2d48d3
mail.david82@gmail.com <mail.david82@gmail.com>
2024-12-19 02:22:19 +0100

    monitor sender

--- monitor/monitor_sender.py
+++ monitor/monitor_sender.py
@@ -7,11 +7,25 @@ def get_free_memory():
     result = subprocess.run(['free', '-m'], stdout=subprocess.PIPE)
     lines = result.stdout.decode('utf-8').split('\n')
     available_memory = int(lines[1].split()[6])
-    return available_memory
+    free_memory = int(lines[1].split()[3])
+    return available_memory, free_memory
+
+# Function to check if a process is running
+def is_process_running(process_name):
+    result = subprocess.run(['pgrep', '-f', process_name], stdout=subprocess.PIPE)
+    return result.returncode == 0
+
+# Get memory values
+available_memory, free_memory = get_free_memory()
+
+# Check if the process is running
+process_status = "OK" if is_process_running("firebaseremoteadmin.py") else "NO"
 
 # Prepare the data to be sent
 data = {
-    "available_memory": get_free_memory()
+    "available_memory": available_memory,
+    "free_memory": free_memory,
+    "process_status": process_status
 }
 
 # Send the data to the endpoint
