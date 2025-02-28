1267e7808f129d6a21a5d8e2beb00bf048f44a3c
mail.david82@gmail.com <mail.david82@gmail.com>
2025-02-28 14:01:11 +0100

    dashboard

--- monitor/start_streamlit_app.sh
+++ monitor/start_streamlit_app.sh
@@ -1,2 +1,2 @@
 source ../venv/bin/activate
-streamlit run monitor_dahboard_streamlit.py --server.port 8501 --server.address 0.0.0.0
+streamlit run monitor_dashboard_streamlit.py --server.port 8501 --server.address 0.0.0.0
