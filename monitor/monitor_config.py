9ef52ad105ffd39c7a7c51d877c0f3ad3ebcbc4f
mail.david82@gmail.com <mail.david82@gmail.com>
2025-03-03 13:22:13 +0100

    dashboard

--- monitor/monitor_config.py
+++ monitor/monitor_config.py
@@ -8,7 +8,7 @@ rack_info = {
     "U2nDDxvRaLm6BNiLhqi6": {"name": "Szalay - M3 Ford", "tzadd": 2,
                              "keys": ["ps_controller_handler", "ps_firebase_main", "ps_firebaseremoteadmin", "ps_smartbox", "ps_firefox_process_count", "memory_available_rate", "sda2_usage", "git_infra_commit_behind", "git_screen_commit_behind", "git_iot_commit_behind"]},
     "uihZJfQfRc5hgQ57uLWw": {"name": "Szalay - Solymár", "tzadd": 2,
-                             "keys": ["ps_firebaseremoteadmin", "ps_smartbox", "ps_firefox_process_count", "memory_available_rate", "sda2_usage", "git_infra_commit_behind", "git_screen_commit_behind", "git_iot_commit_behind"]},
+                             "keys": ["ps_firebaseremoteadmin", "ps_smartbox", "ps_firefox_process_count", "ps_controller_old", "memory_available_rate", "sda2_usage", "git_infra_commit_behind", "git_screen_commit_behind", "git_iot_commit_behind"]},
     "bzAi1DPflIKzg75ipRF3": {"name": "David Graz Teszt HA controller", "tzadd": 2,
                              "keys": ["ps_controller_handler", "ps_firebase_main", "ps_firebaseremoteadmin", "ps_smartbox", "memory_available_rate", "sda2_usage", "git_infra_commit_behind", "git_screen_commit_behind", "git_iot_commit_behind"]},
     "LCFEL7NLIqFX4Cw6GQit": {"name": "Locker Astoria (Controller)", "tzadd": 2,
