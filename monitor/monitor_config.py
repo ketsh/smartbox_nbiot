d25251eed878aa0e46de64273ec70e3893f4be5d
mail.david82@gmail.com <mail.david82@gmail.com>
2025-03-03 13:40:36 +0100

    dashboard

--- monitor/monitor_config.py
+++ monitor/monitor_config.py
@@ -1,6 +1,10 @@
 
 # Dictionary of rack IDs, their names, tzadd values, and keys to be shown
 rack_info = {
+    "placeholder": {"name": "Gödöllői Városi Könyvtár", "tzadd": 2,
+                             "keys": ["ps_controller_handler", "ps_firebase_main", "ps_firebaseremoteadmin",
+                                      "ps_smartbox", "ps_firefox_process_count", "ps_controller_old", "memory_available_rate", "sda2_usage",
+                                      "git_infra_commit_behind", "git_screen_commit_behind", "git_iot_commit_behind"]},
     "L3L2BQwvrjMJfTcEdADW": {"name": "Gödöllői Városi Könyvtár", "tzadd": 2,
                              "keys": ["ps_controller_handler", "ps_firebase_main", "ps_firebaseremoteadmin", "ps_smartbox", "ps_firefox_process_count", "memory_available_rate", "sda2_usage", "git_infra_commit_behind", "git_screen_commit_behind", "git_iot_commit_behind"]},
     "3o3ZcwEuKJ7aM0i5g7RY": {"name": "Akvárium Klub Csomagmegőrző", "tzadd": 2,
