6345e111acdb96c170b545d10b85c809a7ebfb1d
mail.david82@gmail.com <mail.david82@gmail.com>
2025-06-25 17:06:09 +0200

    new rack

--- monitor/monitor_config.py
+++ monitor/monitor_config.py
@@ -18,5 +18,5 @@ rack_info = {
     "LCFEL7NLIqFX4Cw6GQit": {"name": "Locker Astoria (Controller)", "tzadd": 2,
                              "keys": ["ps_controller_handler", "ps_firebase_main", "ps_firebaseremoteadmin", "memory_available_rate", "sda2_usage", "git_infra_commit_behind",  "git_iot_commit_behind"]},
     "tgNFLEu06sNUZPBsUEti": {"name": "Szalay Nissan Esztergom", "tzadd": 2,
-                             "keys": ["ps_firebaseremoteadmin", "ps_smartbox", "ps_firefox_process_count", "ps_controller_old", "memory_available_rate", "sda2_usage", "git_infra_commit_behind", "git_screen_commit_behind", "git_iot_commit_behind"]},
+                             "keys": ["ps_controller_handler", "ps_firebase_main", "ps_firebaseremoteadmin", "ps_smartbox", "ps_firefox_process_count", "memory_available_rate", "sda2_usage", "git_infra_commit_behind", "git_screen_commit_behind", "git_iot_commit_behind"]},
 }
